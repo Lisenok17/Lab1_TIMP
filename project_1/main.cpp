fc6833dea893d088d678c7cdaa3f969275276546
Maks <maks.kuv58@gmail.com>
2022-10-27 11:55:36 +0300

    Update main.cpp

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -1,51 +1,28 @@
 #include <iostream>
-#include <cctype>
-#include <locale>
-#include "modAlphaCipher.h"
-
-
+#include "/home/student/my_cplusplus_programs/Labtimp/Labtimp1/Zadanie1/program1/modAlphaCipher.h"
 using namespace std;
-// проверка, чтобы строка состояла только из прописных букв
-bool isValid(const wstring& s)
+
+void check(const wstring& Text, const wstring& key)
 {
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
-            return false;
-    return true;
+    wstring cipherText;
+    wstring decryptedText;
+    modAlphaCipher cipher(key);
+    cipherText = cipher.encrypt(Text);
+    decryptedText = cipher.decrypt(cipherText);
+    wcout<<L"key=" << key <<endl;
+    wcout<<Text<<endl;
+    wcout<<cipherText<<endl;
+    wcout<<decryptedText<<endl;
+    if (Text == decryptedText)
+        wcout<<L"ok\n";
+    else
+        wcout<<L"err\n";
 }
+
 int main(int argc, char **argv)
 {
     locale loc("ru_RU.UTF-8");
     locale::global(loc);
-    wstring key;
-    wstring text;
-    unsigned op;
-    wcout<<L"Cipher ready. Input key: ";
-    wcin>>key;
-    if (!isValid(key)) {
-        wcerr<<L"key not valid\n";
-        return 1;
-    }
-    wcout<<L"Key loaded\n";
-    modAlphaCipher cipher(key);
-    do {
-        wcout<<L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
-        wcin>>op;
-        if (op > 2) {
-            wcout<<L"Illegal operation\n";
-        } else if (op >0) {
-            wcout<<L"Cipher ready. Input text: ";
-            wcin>>text;
-            if (isValid(text)) {
-                if (op==1) {
-                    wcout<<L"Encrypted text: "<<cipher.encrypt(text)<<endl;
-                } else {
-                    wcout<<L"Decrypted text: "<<cipher.decrypt(text)<<endl;
-                }
-            } else {
-                wcout<<L"Operation aborted: invalid text\n";
-            }
-        }
-    } while (op!=0);
+    check(L"КОТБОРИС", L"МЯУ");
     return 0;
 }
